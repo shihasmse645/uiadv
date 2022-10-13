91bd6d4df15925df0f58d559cb56add9231114a0
shihasmse645 <muhammedshihase@gmail.com>
2022-10-13 11:24:15 +0530

    advanced ui

---
+++ android/app/src/main/kotlin/com/example/uiadv/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uiadv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/responsive_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Screen {
+  Size screenSize;
+
+  Screen(this.screenSize);
+
+  double wp(percentage) {
+    return percentage / 100 * screenSize.width;
+  }
+
+  double hp(percentage) {
+    return percentage / 100 * screenSize.height;
+  }
+
+  double getWidthPx(int pixels) {
+    return (pixels / 3.61) / 100 * screenSize.width;
+  }
+}
